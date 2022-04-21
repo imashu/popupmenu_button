bb5f977c63d4011fa03cc68238d40b5844d82d6b
imashu <debug.ashu@gmail.com>
2022-04-21 19:10:19 +0530

    popup menu

---
+++ android/app/src/main/kotlin/com/example/popupmenu_button/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.popupmenu_button
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+//import 'dart:async';
+
+void main() {
+  runApp(const MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+enum Animals { Cats, Dogs, Bird, Lizard, Fish }
+
+class _MyAppState extends State<MyApp> {
+  Animals _selected = Animals.Cats;
+  String _value = 'Make a Selection';
+  final List<PopupMenuEntry<Animals>> _items = <PopupMenuEntry<Animals>>[];
+
+  //
+
+  @override
+  void initState() {
+    for (Animals animals in Animals.values) {
+      _items.add(PopupMenuItem(
+        child: Text(
+          _getdisplay(animals),
+        ),
+        value: animals,
+      ));
+    }
+
+    super.initState();
+  }
+
+  void _onselected(Animals animals) {
+    setState(() {
+      _selected = animals;
+      _value = 'You Selected ${_getdisplay(animals)}';
+    });
+  }
+
+  String _getdisplay(Animals animals) {
+    int index = animals.toString().indexOf('.');
+    index++;
+    return animals.toString().substring(index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sliders & Indicators'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(32.0),
+        child: Center(
+          child: Row(
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.all(5.0),
+                child: Text(_value),
+              ),
+              PopupMenuButton<Animals>(
+                  child: const Icon(Icons.input),
+                  initialValue: Animals.Cats,
+                  onSelected: _onselected,
+                  itemBuilder: (BuildContext context) {
+                    return _items;
+                  }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
